313f893938f711022c05046ce9939ec2d9c044f1
Adam Hamsik <adam.hamsik@innovatrics.com>
2013-07-24 16:01:54 +0200

    Add tag -l

--- gettags.rb
+++ gettags.rb
@@ -2,12 +2,18 @@
 
 require 'rubygems'
 
-dest_directory='.'
+dest_directory='.git'
 
 dest_commit = ENV['INN_Version_select'] unless ENV['INN_Version_select'].nil?
 
 dest_commit = ARGV[1] unless ARGV[1].nil?
 dest_directory = ARGV[0] unless ARGV[0].nil?
 
+#
+# Setting GIT_DIR to make git comamnds work
+ENV['GIT_DIR'] = dest_directory
+
 puts "Destination Git repository #{dest_directory}"
 puts "Destination Tag #{dest_commit}"
+
+puts "List of existing tags "+`git tag -l`
